a1cd967393b41aa6a9d878bf8727c5e2da306984
hmkimm <lisamin7@naver.com>
2023-04-12 13:43:43 +0900

    add

--- carousel/tab.js
+++ carousel/tab.js
@@ -3,30 +3,27 @@
 // orange 클래스추가
 // 모든 디브에 show 클래스 제거
 // 디브 0에 show 클래스 추가
+var 버튼 = $(".tab-button");
 
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(0).addClass("orange");
+for (let i = 0; i < $("li").length; i++) {
+  버튼.eq(i).on("click", function () {
+    버튼.removeClass("orange");
+    버튼.eq(i).addClass("orange");
     $(".tab-content").removeClass("show");
-    $(".tab-content").eq(0).addClass("show");
+    $(".tab-content").eq(i).addClass("show");
   });
+}
 
-$(".tab-button")
-  .eq(1)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(1).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(1).addClass("show");
-  });
+// 버튼.eq(1).on("click", function () {
+//   버튼.removeClass("orange");
+//   버튼.eq(1).addClass("orange");
+//   $(".tab-content").removeClass("show");
+//   $(".tab-content").eq(1).addClass("show");
+// });
 
-$(".tab-button")
-  .eq(2)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(2).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(2).addClass("show");
-  });
+// 버튼.eq(2).on("click", function () {
+//   버튼.removeClass("orange");
+//   버튼.eq(2).addClass("orange");
+//   $(".tab-content").removeClass("show");
+//   $(".tab-content").eq(2).addClass("show");
+// });
