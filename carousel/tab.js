b8f4f7c4a1d4c915405ca2166080b262a5f0d4df
hmkimm <lisamin7@naver.com>
2023-04-14 23:30:40 +0900

    modify function

--- carousel/tab.js
+++ carousel/tab.js
@@ -5,25 +5,40 @@
 // 디브 0에 show 클래스 추가
 var 버튼 = $(".tab-button");
 
-for (let i = 0; i < $("li").length; i++) {
-  버튼.eq(i).on("click", function () {
-    버튼.removeClass("orange");
-    버튼.eq(i).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(i).addClass("show");
-  });
+//반복문으로 구현하기
+// for (let i = 0; i < $("li").length; i++) {
+//   버튼.eq(i).on("click", function () {
+//     탭열기(i);
+//   });
+// }
+
+function 탭열기(숫자) {
+  버튼.removeClass("orange");
+  버튼.eq(숫자).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(숫자).addClass("show");
 }
 
-// 버튼.eq(1).on("click", function () {
-//   버튼.removeClass("orange");
-//   버튼.eq(1).addClass("orange");
-//   $(".tab-content").removeClass("show");
-//   $(".tab-content").eq(1).addClass("show");
+//이벤트 버블링을 활용해서 구현하기
+const listBtn = document.querySelector("ul");
+const tabOne = document.querySelectorAll("li")[0];
+const tabTwo = document.querySelectorAll("li")[1];
+const tabThree = document.querySelectorAll("li")[2];
+
+//함수로 축약하기
+// listBtn.addEventListener("click", (e) => {
+//   if (e.target == tabOne) {
+//     탭열기(0);
+//   }
+//   if (e.target == tabTwo) {
+//     탭열기(1);
+//   }
+//   if (e.target == tabThree) {
+//     탭열기(2);
+//   }
 // });
 
-// 버튼.eq(2).on("click", function () {
-//   버튼.removeClass("orange");
-//   버튼.eq(2).addClass("orange");
-//   $(".tab-content").removeClass("show");
-//   $(".tab-content").eq(2).addClass("show");
-// });
+//dataset을 활용해서 구현하기 (반복문 말고)
+listBtn.addEventListener("click", (e) => {
+  탭열기(e.target.dataset.id);
+});
